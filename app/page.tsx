fd8ecee520b02561665ab8f5154c0c1e2916a6ca
roshankeumesh-ui <roshankeumesh@gmail.com>
2026-07-18 20:38:06 +0545

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Radhika Suppliers</h1>
+      <p>Welcome to the Business Management System</p>
+    </main>
+  );
+}
